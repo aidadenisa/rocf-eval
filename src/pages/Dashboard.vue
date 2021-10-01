bfe716759feb75b50e79693d81ec954a8d0bd0aa
aida <aida.denisa@gmail.com>
2021-10-01 11:56:00 +0200

    Added preview camera in app, with taking and showing the photo

--- src/pages/Dashboard.vue
+++ src/pages/Dashboard.vue
@@ -1,6 +1,10 @@
 <template>
   <q-page class="flex flex-center">
     <q-btn color="primary" @click="clickk()">Scan new ROCF</q-btn>
+    <img v-if="imageSrc" :src="imageSrc">
+    <q-btn @click="takePicture()">Take picture</q-btn>
+    {{imageSrc}}
+    {{info}}
   </q-page>
 </template>
 
@@ -8,9 +12,63 @@
 
 export default {
   name: 'Dashboard',
+  data() {
+    return {
+      imageSrc: null,
+      info:''
+    }
+  },
+
   methods: {
     clickk() {
+      
       console.log("lol");
+      console.log(navigator)
+      // navigator.camera.getPicture(
+      //   data => { // on success
+      //     this.imageSrc = `data:image/jpeg;base64,${data}`
+      //   },
+      //   () => { // on fail
+      //     this.$q.notify('Could not access device camera.')
+      //   },
+      //   {
+      //     quality: 50,
+      //     destinationType: navigator.camera.DestinationType.DATA_URL,
+      //     encodingType: navigator.camera.EncodingType.JPEG,
+      //     MEDIATYPE: navigator.camera.MediaType.PICTURE,
+      //     sourceType: navigator.camera.PictureSourceType.CAMERA,
+      //     mediaType: navigator.camera.MediaType.PICTURE,
+      //     cameraDirection: navigator.camera.Direction.BACK,
+      //     targetWidth: 300,
+      //     targetHeight: 400
+      //   }
+      // )
+
+      CameraPreview.setPreviewSize({width: 0.5 * window.screen.width, height: 0.5 * window.screen.height});
+
+      let options = {
+        x: 0,
+        y: 0,
+        width: 0.5 * window.screen.width ,
+        height: 0.5 * window.screen.height,
+        camera: CameraPreview.CAMERA_DIRECTION.BACK,
+        toBack: false,
+        tapPhoto: true,
+        tapFocus: false,
+        previewDrag: false,
+        storeToFile: false,
+        disableExifHeaderStripping: false
+      };
+
+      CameraPreview.startCamera(options);
+      CameraPreview.show();
+
+    },
+    takePicture() {
+      //CameraPreview.takePicture(options, successCallback, [errorCallback])
+      CameraPreview.takePicture((base64PictureData) => {
+        this.imageSrc = `data:image/jpeg;base64,${base64PictureData}`
+      });
     }
   }
 }
