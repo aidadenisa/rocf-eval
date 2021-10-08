f3e43c769fe889912f2edc51ff191ca4f9124582
aida <aida.denisa@gmail.com>
2021-10-08 19:41:50 +0200

    Added calculation of the point of touch from the canvas

--- src/pages/SetPoints.vue
+++ src/pages/SetPoints.vue
@@ -1,7 +1,14 @@
 <template>
   <q-page >
-      Please set the 5 refference points:
-    <img  :src="imageSrc" ref="imagePreview">
+      
+    Please set the 5 refference points:
+    <br>
+    <q-btn @click="reset">Toggle points</q-btn>
+    <img :src="imageSrc" ref="imagePreview" v-if="!showPoints">
+    <div v-else style="position:relative">
+        <img :src="imageSrc">
+        <canvas id="overlay"></canvas>
+    </div>
   </q-page>
 </template>
 
@@ -18,37 +25,76 @@ export default {
       paperRatio: 148.5 / 210,
       cropper: {},
       preview: '', 
+      showPoints: false,
+      zoom: {}
     }
   },
   mounted() {
     this.imageSrc = this.$store.state.image;
-    let zoom = new PinchZoom(this.$refs.imagePreview, {});
+    this.zoom = new PinchZoom(this.$refs.imagePreview, {});
+
+    this.$refs.imagePreview.addEventListener( 'touchstart', (event) => {
+        console.log("touch start")
+        console.log(event)
+        console.log (this.zoom)
+        console.log(this.$refs.imagePreview)
+
+        // calculate the point in the zoomed image
+
+        let naturalWidth = this.$refs.imagePreview.naturalWidth;
+        let naturalHeight = this.$refs.imagePreview.naturalHeight;
+
+
+        let t = { 
+            x: this.zoom.offset.x + event.layerX,
+            y: this.zoom.offset.y + event.layerY
+        };
+
+        let scale = window.screen.width / naturalWidth;
+
+        let initialImage = {
+            width: naturalWidth * scale,
+            height: naturalHeight * scale
+        }
+
+        let ratio = {
+            x: t.x / initialImage.width,
+            y: t.y / initialImage.height
+        }
+
+        let finalT = {
+            x: naturalWidth * ratio.x,
+            y: naturalHeight * ratio.y,
+        }
+
+        console.log(finalT);
+
+    })
+
   },
+  methods: {
+      reset() {
+          this.showPoints = !this.showPoints;
+          document.querySelector("div.pinch-zoom-container").style.display = "none";
+          this.zoom.disable();
+      }
+  }
 }
 </script>
 
 <style scoped>
-.image-editor img {
+
+img {
   width: 100%;
 }
 
-.image-editor {
-  width: 100%;
+canvas {
+    width: 100%;
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
 }
 
-.loader {
-  position: absolute;
-  top: 0;
-  bottom: 0;
-  left: 0;
-  right: 0;
-  background: #ffffff;
-  width:100%
-}
-.loader p{ 
-  text-align: center;
-  vertical-align: middle;
-  margin-top: 100px;
-  width: 100%;
-}
 </style>
