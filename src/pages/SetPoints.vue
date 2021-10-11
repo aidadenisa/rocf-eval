d11baf0821faf6c97a34ef5db7403dcbc53ec998
aida <aida.denisa@gmail.com>
2021-10-11 17:50:10 +0200

    Implemented canvas drag of image

--- src/pages/SetPoints.vue
+++ src/pages/SetPoints.vue
@@ -2,13 +2,9 @@
   <q-page >
       
     Please set the 5 refference points:
-    <br>
-    <q-btn @click="reset">Toggle points</q-btn>
-    <img :src="imageSrc" ref="imagePreview" v-if="!showPoints">
-    <div v-else style="position:relative">
-        <img :src="imageSrc">
-        <canvas id="overlay"></canvas>
-    </div>
+    {{info}}
+
+    <canvas id="overlay"></canvas>
   </q-page>
 </template>
 
@@ -31,46 +27,84 @@ export default {
   },
   mounted() {
     this.imageSrc = this.$store.state.image;
-    this.zoom = new PinchZoom(this.$refs.imagePreview, {});
 
-    this.$refs.imagePreview.addEventListener( 'touchstart', (event) => {
-        console.log("touch start")
+    var canvas = document.getElementById('overlay'),
+    context = canvas.getContext('2d');
+    context.imageSmoothingEnabled = false;
+
+    let base_image = new Image();
+    base_image.src = this.imageSrc ;
+
+    canvas.width = window.screen.width * window.devicePixelRatio;
+    // canvas.height = parseInt(window.screen.width * this.paperRatio) * window.devicePixelRatio;
+    canvas.height = window.screen.height * window.devicePixelRatio;
+
+    console.log("canvas dim: ", canvas.width, canvas.height)
+    
+    // ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    let currentPointInOrigin = { x: - canvas.width / 4, y: 0 }
+
+    // console.log(canvas.clientWidth, canvas.clientHeight)
+
+
+    canvas.addEventListener('touchstart', (event) => {
+        event.preventDefault();
+
+
+        event = event.originalEvent || event;
+
+
         console.log(event)
-        console.log (this.zoom)
-        console.log(this.$refs.imagePreview)
 
-        // calculate the point in the zoomed image
+        let moveStartingPoint = {
+            layerX: event.layerX,
+            layerY: event.layerY,
+        };
+        console.log(1);
 
-        let naturalWidth = this.$refs.imagePreview.naturalWidth;
-        let naturalHeight = this.$refs.imagePreview.naturalHeight;
+        let scale = canvas.width / base_image.width;
+
+        console.log(scale)
+
+    
+        const moveHandler = (event) => {
+            event.preventDefault();
+            console.log("move");
+            // event = event.originalEvent || event;
+
+            //update current position
+            currentPointInOrigin.x =  currentPointInOrigin.x - (moveStartingPoint.layerX - event.layerX) / ( (1/scale) * 2 * window.devicePixelRatio);
+            currentPointInOrigin.y =  currentPointInOrigin.y - (moveStartingPoint.layerY - event.layerY) / ( (1/scale) * 4 * window.devicePixelRatio);
 
 
-        let t = { 
-            x: this.zoom.offset.x + event.layerX,
-            y: this.zoom.offset.y + event.layerY
+            //if current position if outside of bounds, set it to the bounds
+            currentPointInOrigin.x = currentPointInOrigin.x > 0 ? 0 : currentPointInOrigin.x;
+            currentPointInOrigin.x = (- currentPointInOrigin.x) + canvas.clientWidth > canvas.width ? - (canvas.width - canvas.clientWidth) : currentPointInOrigin.x;
+
+            currentPointInOrigin.y = currentPointInOrigin.y > 0 ? 0 : currentPointInOrigin.y;
+            // currentPointInOrigin.y = currentPointInOrigin.y + canvas.clientHeight > canvas.height ? (canvas.height - canvas.clientHeight) : currentPointInOrigin.y;
+            
         };
 
-        let scale = window.screen.width / naturalWidth;
-
-        let initialImage = {
-            width: naturalWidth * scale,
-            height: naturalHeight * scale
+        const stopHandler = (event) => {
+            event.preventDefault();
+            canvas.removeEventListener('touchmove', moveHandler);
+            canvas.removeEventListener('touchend', stopHandler);
         }
 
-        let ratio = {
-            x: t.x / initialImage.width,
-            y: t.y / initialImage.height
-        }
+        canvas.addEventListener('touchmove', moveHandler);
+        canvas.addEventListener('touchend', stopHandler);
 
-        let finalT = {
-            x: naturalWidth * ratio.x,
-            y: naturalHeight * ratio.y,
-        }
+    });
 
-        console.log(finalT);
-
-    })
-
+    base_image.onload = () => {
+        setInterval(() => {
+            context.clearRect(0, 0, canvas.width, canvas.height);
+            context.drawImage(base_image, currentPointInOrigin.x, currentPointInOrigin.y);
+        }, 50)
+    }
+    
   },
   methods: {
       reset() {
@@ -90,11 +124,6 @@ img {
 
 canvas {
     width: 100%;
-    position: absolute;
-    top: 0;
-    bottom: 0;
-    left: 0;
-    right: 0;
 }
 
 </style>
